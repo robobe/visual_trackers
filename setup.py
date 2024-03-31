94852f8aa5561abb58d4dfcfb55f54eed497c2b3
robo <robo2020@gmail.com>
2024-03-31 18:37:10 +0300

    lk

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='visual_trackers',
+    version='0.1',
+    description='Description of my package',
+    packages=find_packages(),    
+    install_requires=['numpy >= 1.11.1'],
+)
